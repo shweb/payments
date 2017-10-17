fd66795c465dd10a3293f67e4ded4ff3e8838559
koera-sam <tolotrarazafindrabe@gmail.com>
2017-10-17 08:45:29 +0200

    _

--- resources/views/qrcode.blade.php
+++ resources/views/qrcode.blade.php
@@ -10,7 +10,20 @@
     <link href="{{url('/')}}/public/css/bootstrap.min.css" rel="stylesheet">
     <script src="{{url('/')}}/public/js/jquery.min.js"></script>
     <script src="{{url('/')}}/public/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="{{url('/')}}/public/css/style.css">
     <title>微信支付样例-退款</title>
+
+    {{--<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mediaqueries.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>--}}
 </head>
 <body>
 <header>
