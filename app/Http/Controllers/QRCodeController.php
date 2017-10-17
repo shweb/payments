b944038c7291a6ae2c6209ce691ba8757b67f0eb
koera-sam <tolotrarazafindrabe@gmail.com>
2017-10-17 11:00:43 +0200

    _

--- app/Http/Controllers/QRCodeController.php
+++ app/Http/Controllers/QRCodeController.php
@@ -29,7 +29,7 @@ class QRCodeController extends Controller
                 $input->SetTotal_fee("1");
             $input->SetOut_trade_no(WxPayConfig::MCHID . date("YmdHis"));
             $input->SetTime_start(date("YmdHis"));
-            $input->SetTime_expire(date("YmdHis", time() + 1200));
+//            $input->SetTime_expire(date("YmdHis", time() + 1200));
 //            dd('time start : ' . date("YmdHis") . ' time end : ' . date("YmdHis", time() + 600));
             $input->SetGoods_tag("ULTRAVIOLET");
             $input->SetNotify_url("http://uvbypp-mmbund-payments.com/payments/wechat/notify");
