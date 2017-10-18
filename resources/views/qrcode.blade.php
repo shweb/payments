e66eb595bdf9f3618af79e61051037edb46f538c
koera-sam <tolotrarazafindrabe@gmail.com>
2017-10-18 08:49:45 +0200

    _

--- resources/views/qrcode.blade.php
+++ resources/views/qrcode.blade.php
@@ -47,11 +47,11 @@
     <div class="container">
         <div class="row information">
             <div class="col-md-6 col-xs-12">
-                <p class="text-left-xs text-left-sm text-right-md"><label>Order number :</label> <?php echo $_SESSION['booking_id'] ?></p>
+                <p class="text-left-xs text-left-sm text-right-md"><label>Order number :</label> {{\Illuminate\Support\Facades\Session::get('booking_id')}}</p>
             </div>
             <div class="col-md-6 col-xs-12 ">
                 <p class="text-left-xs text-left-sm text-right-md"><label>Total Price
-                        : </label> <?php echo $_SESSION['amount'] ?>
+                        : </label> {{\Illuminate\Support\Facades\Session::get('amount')}}
                 <p>
             </div>
         </div>
