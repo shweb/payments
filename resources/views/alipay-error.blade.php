e4e0df3c5611da582d902c7cf67c190de8158a8a
tolotravalery <valeryharisoanitolotra@gmail.com>
2019-03-07 14:53:15 +0300

    view success et error alipay

---
+++ resources/views/alipay-error.blade.php
@@ -0,0 +1,21 @@
+@extends("template")
+@section("contenu")
+    <section>
+        <div class="container" id="failed">
+            <div class="separated hidden-xs "></div>
+
+            <div class="col-md-8 col-md-offset-2">
+                <div class="bg-custom2">
+                    <div class="alert alert-warning">
+                        Payment failed !!.
+                    </div>
+                    <div class="row">
+                        <!--<div class="col-md-6 col-md-offset-3 center">
+                            <button type="button" class="btn btn-success btn-lg ">Confirm</button>
+                        </div>-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/alipay-success.blade.php
@@ -0,0 +1,32 @@
+@extends("template")
+@section("contenu")
+    <section>
+        <div class="container" id="success">
+            <div class="separated hidden-xs "></div>
+
+            <div class="col-md-8 col-md-offset-2">
+                <div class="bg-custom2">
+                    <div class="alert alert-success">
+                        Payment successful !!!<br/>
+                    </div>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3 center">
+                            <p style="font-size: 16px;">You will be redirected to uvbypp.cc soon....</p>
+                            <p style="font-size: 16px;" id="counter">5</p>
+                        </div>
+                    </div>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
